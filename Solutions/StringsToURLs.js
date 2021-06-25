38cfd188eeee1f02ea7595fe3805a2c9df4cec07
cass1618 <cass16180@gmail.com>
2021-06-25 09:47:30 -0700

    Add non recursive solution for first prompt

---
+++ Solutions/StringsToURLs.js
@@ -0,0 +1,13 @@
+function toUrl(string) {
+    return string.split(" ").join("%20");
+}
+
+//RECURSION
+
+//base case: string with no spaces and all %20s
+//termination case: input other than a string
+
+
+function toUrlR(string) {
+
+}
