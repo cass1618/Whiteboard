5529ffe3f1fe387548c4f5adb2d5bdb5c5183cfc
cass1618 <cass16180@gmail.com>
2021-06-25 12:41:20 -0700

    Add solution for filter version of deduping

--- Solutions/ArrayDeduping.js
+++ Solutions/ArrayDeduping.js
@@ -1,5 +1,5 @@
-let inAr = [7, 9, "hi", 12, "hi", 7, 53]
-Output: [7, 9, "hi", 12, 53]
+let array = [7, 9, "hi", 12, "hi", 7, 53]
+// Output: [7, 9, "hi", 12, 53]
 
 function deDupe(array) {
     let noDupes = [];
@@ -12,6 +12,13 @@ function deDupe(array) {
 }
 
 
-const deDupeRecursive = (array) => {
+//If the element is in the array more than once, it will exist at 2 or more indices
+//Use filter to loop through the array and only return elements that do not exist at any other index
+//Once you get to the duplicate, array.indexOf will provide the index of the first copy, which will not match
 
+
+const deDupeFilter = (array) => {
+    return array.filter((e, i) => {
+        return array.indexOf(e) === i;
+});
 }
