e409d38902ac4c49317811bd64053ff93d25d622
cass1618 <cass16180@gmail.com>
2021-06-25 10:03:46 -0700

    :construction: Recursive solution to first prompt

--- Solutions/StringsToURLs.js
+++ Solutions/StringsToURLs.js
@@ -7,7 +7,17 @@ function toUrl(string) {
 //base case: string with no spaces and all %20s
 //termination case: input other than a string
 
+//Remove first space it gets to and then remove first space it gets to and then . .
 
-function toUrlR(string) {
-
+function toUrlR(string, index = 0) {
+    if(string[index] != undefined) {
+        if(string[index]===" ") {
+            //replace the space with the %20 thing here
+            return toUrlR(string, index + 1);
+        } else {
+            return toUrlR(string, index + 1);
+        }
+    } else {
+        return string;
+    }
 }
